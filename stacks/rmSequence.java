c900c87348eaf10716cc124594bafab8f6496997
sanskaryo <sanskartodwal@gmail.com>
2024-07-15 16:39:18 +0530

    stack questions

---
+++ stacks/rmSequence.java
@@ -0,0 +1,33 @@
+
+import java.util.Arrays;
+import java.util.Stack;
+
+
+
+public class rmSequence {
+
+    public static int[] remove(int[] arr) {
+        int n = arr.length;
+        Stack<Integer> st = new Stack<>();
+        for(int i =0;i<n;i++) {
+            if(st.size() ==0 || st.peek()!=arr[i])
+                st.push(arr[i]);
+            else if(arr[i] == st.peek()) {
+                if(i==n-1 || arr[i]!=arr[i+1]) st.pop();
+            }
+        }
+        int[] res = new int[st.size()];
+        int m  = res.length ;
+        for(int i =m-1; i>0; i--) {
+            res[i] = st.pop();
+        }
+        return res;
+    }
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 2, 3, 4, 4, 5};
+        int[] res = remove(arr);
+        System.out.println(Arrays.toString(res));
+    }
+    }
+    
+
